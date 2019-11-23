0d73f51bb6860a2dbf84429c8bae3cf108633f1f
Vinnicius Leandro <vinniciuslsp@gmail.com>
2019-11-22 23:33:26 -0300

    Add files via upload

--- projeto_mobile_ect/src/screens/DetalhesLivros.js
+++ projeto_mobile_ect/src/screens/DetalhesLivros.js
@@ -2,8 +2,6 @@ import React, { Component } from 'react';
 import { View, FlatList, Text, Button, StyleSheet } from 'react-native';
 import { NavigationEvents } from 'react-navigation';
 import firestore from '@react-native-firebase/firestore';
-import { createStackNavigator } from 'react-navigation-stack';
-import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const extractKey = ({ id }) => id
 
@@ -46,67 +44,41 @@ export default class Home extends Component {
             })
     }
 
-
     renderItem = ({ item }) => {
         return (
             <View style={styles.row}>
-                 <TouchableOpacity onPress={() => this.props.navigation.navigate('DetalhesLivros')}>
-
                 <Text style={{}}> Titulo: {item.titulo} </Text>
                 <Text> Autor: {item.autor}</Text>
                 <Text> Telefone Doador: {this.props.navigation.getParam('phone','+55 ')} </Text>
-                
-                </TouchableOpacity>
             </View>
         )
     }
 
     render() {
-
         return (
 
             <View style={styles.container}>
                 <NavigationEvents onDidFocus={() => this.getLivros()} />
-               
                 <FlatList
                     data={this.state.livros}
+
                     renderItem={this.renderItem}
                     keyExtractor={extractKey}
                 />
-
-                <TouchableOpacity style={styles.button} 
-                onPress={ () => this.props.navigation.navigate('Cadastro')}>
-
-                <Text style={styles.buttonsignin}> + </Text>
-
-                </TouchableOpacity>
             </View>
         );
     }
 }
-
     
 const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
     row: {
-        padding: 10,
-        marginBottom: 10,
+        padding: 20,
+        marginBottom: 20,
         backgroundColor: '#cce6ff',
-        elevation: 4
-    },
-    button: {
-        margin: 20,
-        borderRadius: 50,
-        height: 90,
-        marginLeft: 300,
-        backgroundColor: '#0066ff',
-      },
-    buttonsignin: {
-        fontSize: 60,
-        fontWeight: '500',
-        color: '#ffffff',
-        textAlign: 'center'
+        elevation: 15,
+        alignItems: 'center'
     }
 })
--- projeto_mobile_ect/src/screens/MenuPrincipal.js
+++ projeto_mobile_ect/src/screens/MenuPrincipal.js
@@ -2,15 +2,11 @@ import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
 
 import Home from './Home'
-import Cadastro from './Cadastro'
 import MeusDados from './MeusDados'
-import Page1 from './Page1'
 
 const drawerNavigation = createDrawerNavigator({
     Home,
-    Cadastro,
-    MeusDados,
-    Page1
+    MeusDados
     
 });
 
